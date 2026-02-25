d46926b55559be79f2ed30ad3b54cac1831dc08f
divyansharma001 <divyansharma001@users.noreply.github.com>
2026-02-25 11:54:48 +0530

    fix: apply clang-format to tm_config.cpp

--- src/Texmacs/Server/tm_config.cpp
+++ src/Texmacs/Server/tm_config.cpp
@@ -264,13 +264,11 @@ system_kbd_initialize (hashmap<string, tree>& h) {
     h ("<gtr>")    = "<#3E>";
   }
   else if (gui_is_qt ()) {
-    h ("S-")       = localize ("Shift::keyboard", true);
-    h ("C-")       = localize ("Ctrl::keyboard", true);
-    h ("A-")       = localize ("Alt::keyboard", true);
-    if (os_win ())
-      h ("M-")     = localize ("Win::keyboard", true);
-    else
-      h ("M-")     = localize ("Super::keyboard", true);
+    h ("S-")= localize ("Shift::keyboard", true);
+    h ("C-")= localize ("Ctrl::keyboard", true);
+    h ("A-")= localize ("Alt::keyboard", true);
+    if (os_win ()) h ("M-")= localize ("Win::keyboard", true);
+    else h ("M-")= localize ("Super::keyboard", true);
     h ("H-")       = localize ("Hyper::keyboard", true);
     h ("windows")  = localize ("Windows::keyboard");
     h ("capslock") = localize ("Capslock::keyboard");
